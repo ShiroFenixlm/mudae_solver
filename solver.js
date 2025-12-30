919d29e660728f6b8624c83af3cef94e51cfbcb9
ShiroFenixlm <30911462+ShiroFenixlm@users.noreply.github.com>
2025-12-30 19:45:29 -0400

    alright

--- solver.js
+++ solver.js
@@ -11,4 +11,21 @@ const STATES = {
 
 let board = Array.from({length: SIZE}, () =>
     Array.from({length: SIZE}, () => STATES.UNKNOWN)
-)
+);
+
+function render() {
+    const boardDiv = document.getElementById("board");
+    boardDiv.innerHTML = "";
+
+    for (let y = 1; y <= SIZE; y++){
+        for (let x = 1; x <= SIZE; x++){
+            const cell = document.createElement("div");
+            cell.className = "cell";
+
+            if (board[x][y] !== STATES.UNKNOWN){
+                cell.classList.add(board[x][y])
+            }
+        }
+        
+    }
+}
